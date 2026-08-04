781760882459d36d5932755b58caf6acb2504a1a
Kukiat <-g>
2026-08-04 17:02:49 +0700

    Restore response type declaration for users entity in users plugin

--- apps/api/src/modules/users/users.plugin.ts
+++ apps/api/src/modules/users/users.plugin.ts
@@ -80,7 +80,7 @@ export const usersPlugin = new Elysia({ prefix: "/users", tags: ["users"] })
         );
       }
     },
-    { response: "users.res.list" },
+    // { response: "users.res.list" },
   )
 
   .get("/", ({ usersService }) => ok(usersService.list()), {
@@ -197,6 +197,6 @@ export const usersPlugin = new Elysia({ prefix: "/users", tags: ["users"] })
     {
       params: "users.params.external",
       body: "users.action",
-      // response: "users.res.entity",
+      response: "users.res.entity",
     },
   );
